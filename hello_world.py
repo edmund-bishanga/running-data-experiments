6b3ee70d29ab69bfe2fc2c0fff4702a29a4c4044
Edmund Muzoora Bishanga <me.bishanga03@cantab.net>
2020-06-15 17:03:07 +0100

    Hello World: python 3
    HAPPY FATHERS DAY 2020:
    
    EMMANUEL: GOD WITH US
    CHRIST JESUS.
    
    + Read ya Bible
    + Pray every Today
    + HALLELUJAH... Forevermore...

---
+++ hello_world.py
@@ -0,0 +1,20 @@
+#usr/bin/python
+
+print("Hello world")
+print("GOD LOVES YOU")
+print("John 3:16\n")
+
+print("CHRIST JESUS: EMMANUEL...")
+print("Full of TRUTH & GRACE")
+print("John 1:17\n")
+
+print("Love God: with ALL you are... + the kitchen sink!")
+print("Love ya Neighbour: as you already love ya self...")
+print("ENJOY THE DISCIPLESHIP PROCESS")
+print("HALLELUJAH!!!\n")
+
+print("just another pilgrim: Edmund Muzoora BISHANGA")
+print("#TeamEMMANUELForever")
+print("#YNWA")
+
+
