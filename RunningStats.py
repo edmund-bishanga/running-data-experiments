6a65c395dbe7b39143b2d7bdf8931d14c37d5df1
Edmund Muzoora Bishanga <me.bishanga03@cantab.net>
2020-07-11 18:55:29 +0100

    Initial thoughts on Running Stats: objectives

---
+++ RunningStats.py
@@ -0,0 +1,22 @@
+#!usr/bin/python
+
+# exploring various statistical information keys from my running data
+# average pace
+# total mileage
+# total time
+# VO2Max estimate
+# running economy analysis
+
+# 1. Get Run instance Data
+#    From a file, I/O input.
+#    Possible formats: .csv, .txt
+#    Parsing Data
+#    into Appropriate Data Structure e.g. Dictionaries, Named Tuples, Lists etc.
+
+# 2. Calculate basic running Stats
+
+# 3. Do Running Economy Analysis
+
+# 4. Provide Recommendations
+
+
