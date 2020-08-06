7d96308d4a244ce478559b657e245836413dfb2a
Edmund Muzoora Bishanga <me.bishanga03@cantab.net>
2020-08-06 11:51:33 +0100

    Sample running data: parkrun results

--- RunningStats.py
+++ RunningStats.py
@@ -2,6 +2,9 @@
 
 import json
 import parser
+from pprint import pprint
+import sys
+
 # exploring various statistical information keys from my running data
 # average pace
 # total mileage
@@ -14,6 +17,22 @@ import parser
 #    Possible formats: .csv, .txt
 #    Parsing Data
 #    into Appropriate Data Structure e.g. Dictionaries, Named Tuples, Lists etc.
+runfile = "./ParkRun_Pocket_Bishanga_Edmund.csv"
+
+def getFileContents(filepath):
+    with open(filepath, 'r') as f:
+        fdata = f.readlines()
+    pprint({filepath: fdata})
+    return fdata
+
+rundata = getFileContents(runfile)
+
+# parse data -> appropriate data structure
+headings = list()
+if rundata:
+    for word in rundata[0].strip('\n').split(','):
+        headings.append(word)
+pprint(headings)
 
 # 2. Calculate basic running Stats
 
