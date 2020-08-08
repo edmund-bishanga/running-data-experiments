ceff90b4ddad14bd28ea80693330e9fb45b55934
Edmund Muzoora Bishanga <me.bishanga03@cantab.net>
2020-08-08 01:25:38 +0100

    Basic stats using scipy: pocket parkrun

--- RunningStats.py
+++ RunningStats.py
@@ -3,6 +3,7 @@
 import json
 import parser
 from pprint import pprint
+from scipy import stats
 import sys
 
 # exploring various statistical information keys from my running data
@@ -22,20 +23,47 @@ runfile = "./ParkRun_Pocket_Bishanga_Edmund.csv"
 def getFileContents(filepath):
     with open(filepath, 'r') as f:
         fdata = f.readlines()
-    pprint({filepath: fdata})
+    # pprint({filepath: fdata})
     return fdata
 
 rundata = getFileContents(runfile)
 
 # parse data -> appropriate data structure
-headings = list()
-if rundata:
-    for word in rundata[0].strip('\n').split(','):
-        headings.append(word)
-pprint(headings)
+if not rundata:
+    print("{} seems empty or it's data inaccessible".format(runfile))
+    exit(1)
+units = dict()
+labels = list()
+for header in rundata.pop(0).strip('\n').split(','):
+    if header:
+        label = header.split(' ')[0]
+        unit = header.split(' ')[1].strip('(').strip(')') if len(header.split(' ')) == 2 else ''
+        units.update({label:unit})
+        labels.append(label)
+print('UNITS:'); pprint(units, width=400)
+print('LABELS'); pprint(labels, width=400)
+
+rows = list()
+for row in rundata:
+    if len(row.strip('\n').strip(',')) > 0:
+        vals = [val for val in row.strip('\n').split(',')]
+        lrow = dict(zip(labels, vals))
+        rows.append(lrow)
+print('ROWS:'); pprint(rows, width=400)
 
 # 2. Calculate basic running Stats
+times = list()
+event_search_str = 'pocket parkrun'
+for row in rows:
+    pprint(row)
+    if event_search_str in row.get('venue'):
+        str_time = row.get('time')
+        hr, mm, ss = tuple(str_time.split(':'))
+        duration = float((int(hr) * 3600 + int(mm) * 60 + int(ss)) / 60)
+        times.append(duration)
+print('TIMES: seconds'); pprint(times, width=400)
 
+pprint(stats.describe(times))
 # 3. Do Running Economy Analysis
 
 # 4. Provide Recommendations
