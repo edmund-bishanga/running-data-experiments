c7eea386682ebaec9408239e501f00a82272c5f8
Edmund M BISHANGA <me.bishanga03@cantab.net>
2022-06-18 23:51:46 +0100

    park_run_stats_experiments: readability and line length improvemnts + py3_print_f_format + minor syntax updates

--- park_run_stats_experiments.py
+++ park_run_stats_experiments.py
@@ -65,7 +65,8 @@ def get_csv_column_raw_data(csv_file, heading, max_length=100):
     # validate requested heading
     heading_keys = next(row_items)
     print(f'\nDEBUG: headings: {heading_keys}')
-    assert heading in heading_keys, f'MissingHeadingErr: {heading} not in {heading_keys}'
+    err_msg = f'MissingHeadingErr: {heading} not in {heading_keys}'
+    assert heading in heading_keys, err_msg
 
     # get requested column data: raw
     row_dicts = (dict(zip(heading_keys, vals)) for vals in row_items)
@@ -76,32 +77,39 @@ def get_csv_column_raw_data(csv_file, heading, max_length=100):
     # sum_raw_data = sum(unltd_raw_data_gen)
 
     # harvest up to the max_length: to a list
-    ltd_raw_data, len_raw_data = collect_buffered_raw_data(unltd_raw_data_gen, max_length)
+    ltd_raw_data, len_raw_data = collect_buffered_raw_data(
+        unltd_raw_data_gen, max_length
+    )
 
     # return a dictionary: of raw data
     col_raw_data = {heading: ltd_raw_data, 'length': len_raw_data}
     return col_raw_data
 
 def transform_raw_to_int(raw_parkrun_times, times_heading):
-    seconds_parkrun_times = raw_parkrun_times
-    for i, val in enumerate(seconds_parkrun_times.get(times_heading)):
+    parkrun_times_seconds = raw_parkrun_times
+    for i, val in enumerate(parkrun_times_seconds.get(times_heading)):
         s_val = ParkRunner.parse_race_timestr_to_seconds(val)
-        seconds_parkrun_times.get(times_heading)[i] = s_val
-    return seconds_parkrun_times
+        parkrun_times_seconds.get(times_heading)[i] = s_val
+    return parkrun_times_seconds
 
 def main():
     """ Do ParkRun Data Experiments. """
 
     # 1: PROCESS LARGE CSV DATA: USING GENERATORS, DICTS
-    parkrun_csv_file = './test_data/park_run_raw_data_bishanga_edmund.csv'
+    # parkrun_csv_file = './test_data/park_run_raw_data_bishanga_edmund.csv'
+    parkrun_csv_file = './test_data/park_run_raw_data_bishanga_edmund_todos.csv'
     # get raw column data: 'times'
     times_heading = 'time (hh:mm:ss)'
-    raw_parkrun_times = get_csv_column_raw_data(parkrun_csv_file, times_heading, max_length=200)
+    raw_parkrun_times = get_csv_column_raw_data(
+        parkrun_csv_file, times_heading, max_length=200
+    )
     # transform the raw data: for statistical informational analysis
     # times: str -> int|seconds
-    seconds_parkrun_times = transform_raw_to_int(raw_parkrun_times, times_heading)
-    print('\nDEBUG: seconds_parkrun_times')
-    pprint(seconds_parkrun_times)
+    parkrun_times_seconds = transform_raw_to_int(
+        raw_parkrun_times, times_heading
+    )
+    print('\nDEBUG: parkrun_times_seconds')
+    pprint(parkrun_times_seconds)
 
     # 2: PROCESS CSV DATA: USING PANDAS, ITERATORS, MATPLOTLIB
     # parkrun_csv_file = "./test_data/half_marathon_bishanga_edmund.csv"
@@ -114,14 +122,16 @@ def main():
 
     # 2: A: parse data -> appropriate data structure
     if not rundata:
-        print("{} seems empty or it's data inaccessible".format(parkrun_csv_file))
+        print(f"{parkrun_csv_file}: seems empty or its data inaccessible")
         sys.exit(1)
     units = dict()
     labels = list()
     for header in rundata.pop(0).strip('\n').split(','):
         if header:
             label = header.split(' ')[0]
-            unit = header.split(' ')[1].strip('(').strip(')') if len(header.split(' ')) == 2 else ''
+            unit = ''
+            if len(header.split(' ')) == 2:
+                unit = header.split(' ')[1].strip('(').strip(')')
             units.update({label:unit})
             labels.append(label)
     rows = list()
@@ -155,13 +165,16 @@ def main():
             if event_name in row.get('venue'):
                 str_time = row.get(event_y_axis)
                 hr, mm, ss = tuple(str_time.split(':'))
-                duration = round(float((int(hr) * 3600 + int(mm) * 60 + int(ss)) / 60), 1)
+                duration = round(
+                    float((int(hr) * 3600 + int(mm) * 60 + int(ss)) / 60),
+                    1
+                )
                 y_values.append(duration)
                 x_values.append(row.get(event_x_axis))
     coordinates = tuple(zip(x_values, y_values))
-    print('title: {}'.format(event_name))
-    print('x_axis: {}'.format(event_x_axis))
-    print('y_axis: {}'.format(event_y_axis))
+    print(f'title: {event_name}')
+    print(f'x_axis: {event_x_axis}')
+    print(f'y_axis: {event_y_axis}')
     print('Coordinates: ')
     pprint(coordinates, width=1600)
     # 2: B: i: output basic stats summary
@@ -174,13 +187,13 @@ def main():
     pyplot.show()
     # 2: B: iii: Plot normal distribution
     mean = round(numpy.mean(y_values), 2)
-    print('mean: {}'.format(mean))
+    print(f'mean: {mean}')
 
     std_dev = round(numpy.std(y_values), 4)
-    print('std_dev: {}'.format(std_dev))
+    print(f'std_dev: {std_dev}')
 
     variance = round(std_dev**2, 2)
-    print('variance: {}'.format(variance))
+    print(f'variance: {variance}')
 
     x_data = sorted(y_values)
     # pylint: disable=line-too-long
@@ -191,7 +204,7 @@ def main():
     )
     pyplot.xlabel('run times')
     pyplot.ylabel('likelihood')
-    pyplot.title('{event}: estimated Normal|Gaussian distribution'.format(event=event_name))
+    pyplot.title(f'{event_name}: estimated Normal|Gaussian distribution')
     pyplot.show()
 
     # 3. Do Running Economy Analysis
